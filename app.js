2d5ffa7e38c14904d99f6ba37e1266e0dff4eacf
siddharthagarwal112000@gmail.com <43958102+Siddharth-Agrwal@users.noreply.github.com>
2020-08-16 09:41:10 +0530

    Basic Folder Structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express       = require('express'),
+      app           = express(),
+      bodyParser    = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+
+
+// Basic Route
+app.get("/", function(req, res){
+  res.render('landing');
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Connected");
+});
